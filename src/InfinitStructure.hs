d9c711ebedda72d65a0ef5f8c7cdbc2e7cff603f
Alex-Diez <alexvic.amatory@gmail.com>
2016-03-21 15:02:25 +0200

    infinite structure

---
+++ src/InfinitStructure.hs
@@ -0,0 +1,67 @@
+module InfinitStructure where
+
+import Data.List
+
+numbers :: [Integer]
+numbers = 0:map (1+) numbers
+
+take' n [] = []
+take' 0 list = []
+take' n (x:xs) = x:take' (n-1) xs
+
+data BinTree bt = Empty
+                  | Node bt (BinTree bt) (BinTree bt)
+                        deriving(Eq,Ord)
+
+treeFromList :: (Ord o) => [o] -> BinTree o
+treeFromList [] = Empty
+treeFromList (x:xs) = Node x (treeFromList (filter (<x) xs))
+                             (treeFromList (filter (>x) xs))
+
+instance (Show bt) => Show (BinTree bt)
+    where
+        show t = "< " ++ replace '\n' "\n: " (treeshow "" t)
+            where
+                treeshow pref Empty = ""
+                treeshow pref (Node x Empty Empty) = (pshow pref x)
+                treeshow pref (Node x left Empty) = (pshow pref x) ++ "\n" ++
+                                                    (showSon pref "`--" "   " left)
+                treeshow pref (Node x Empty right) =
+                              (pshow pref x) ++ "\n" ++
+                              (showSon pref "`--" "   " right)
+                treeshow pref (Node x left right) =
+                              (pshow pref x) ++ "\n" ++
+                              (showSon pref "|--" "|  " left) ++ "\n" ++
+                              (showSon pref "`--" "   " right)
+                showSon pref before next t = pref ++ before ++ treeshow (pref ++ next) t
+                pshow pref x = replace '\n' ("\n" ++ pref) (show x)
+                replace c new string = concatMap (change c new) string
+                    where
+                        change c new x
+                            | x == c = new
+                            | otherwise = x:[]
+
+nullTree = Node 0 nullTree nullTree
+
+treeTakeDepth _ Empty = Empty
+treeTakeDepth 0 _     = Empty
+treeTakeDepth n (Node x left right) = let
+          nl = treeTakeDepth (n-1) left
+          nr = treeTakeDepth (n-1) right
+          in
+              Node x nl nr
+
+iTree = Node 0 (dec iTree) (inc iTree)
+    where
+            dec (Node x l r) = Node (x-1) (dec l) (dec r)
+            inc (Node x l r) = Node (x+1) (inc l) (inc r)
+
+treeMap :: (a -> b) -> BinTree a -> BinTree b
+treeMap f Empty = Empty
+treeMap f (Node x left right) = Node (f x)
+                                     (treeMap f left)
+                                     (treeMap f right)
+
+infTreeTwo :: BinTree Int
+infTreeTwo = Node 0 (treeMap (\x -> x - 1) infTreeTwo)
+                    (treeMap (\x -> x + 1) infTreeTwo)
--- src/Main.hs
+++ src/Main.hs
@@ -3,32 +3,9 @@ module Main where
 import Functions
 import Types
 import Tree
+import InfinitStructure
 
 main = do
-    functions
-    types
-
-    print $ Tree.treeFromList [15,34,1,3,5,7,2,4,23,45,8]
-
-    where
-        functions = do
-            print(evenSumInt [1..100])
-            print(evenSumIntegral [1..100])
-            print(evenSumMatch [1..100])
-            print(evenSumEta [1..100])
-            print(evenSumHof [1..100])
-            print(evenSumHofFold [1..100])
-            print(evenSumHofEagerFold [1..100])
-            print(evenSumLambdas [1..100])
-            print(evenSumLambdasOp [1..100])
-            print(evenSumLambdasEta [1..100])
-
-            print(squareEvenSum [1..100])
-            print(squareEvenSum' [1..100])
-            print(squareEvenSum'' [1..100])
-
-        types = do
-            print(showInfos name color)
-            print(showInfosTypes nameType colorType)
-            print(z)
-            print(c)
+    print $ take' 10 numbers
+    print $ treeTakeDepth 4 nullTree
+    print $ treeTakeDepth 4 infTreeTwo
