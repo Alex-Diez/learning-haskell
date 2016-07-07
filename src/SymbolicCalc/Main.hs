c64c18b338c6ddd99390a00e57338bad1603865f
Alex-Diez <alexvic.amatory@gmail.com>
2016-07-07 18:06:18 +0300

    complete symbolic calc

---
+++ src/SymbolicCalc/Main.hs
@@ -0,0 +1,23 @@
+module SymbolicCalc.Main where
+
+    import qualified Data.Map as Map
+    import SymbolicCalc.Lexer
+    import SymbolicCalc.Parser
+    import SymbolicCalc.Evaluator
+
+    main :: IO ()
+    main = do
+        loop (Map.fromList [("pi", pi), ("e", exp 1.0)])
+
+    loop table = do
+        str <- getLine
+        if null str
+            then return ()
+            else
+                let tokens = tokenize str
+                    node = parse tokens
+                    Evaluator action = evaluate node
+                    (val, table') = action table
+                in do
+                    print val
+                    loop table'
